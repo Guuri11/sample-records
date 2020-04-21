dab02e1d4d75a8766891af7e90a0d223994e87ee
Guuri11 <sergio.gurillo11@gmail.com>
2020-04-21 18:47:07 +0200

    Implementado React JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import '../css/app.css';
 
-ReactDOM.render(<Router><h1>Hola mundo</h1>/Router>, document.getElementById('root'));
+ReactDOM.render(<Router><h1>Hola mundo</h1></Router>, document.getElementById('root'));
